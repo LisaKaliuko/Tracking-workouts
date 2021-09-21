1758d1874489e8401da5438105bc8cdd82413cb1
Lizaveta Kaliuka <lisakaliuko@gmail.com>
2021-09-21 12:08:05 +0300

    add firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAcOSMZuSDHUMYiq5p4y_JB2IEAf1toipU',
+  authDomain: 'tracking-app-c58fb.firebaseapp.com',
+  projectId: 'tracking-app-c58fb',
+  storageBucket: 'tracking-app-c58fb.appspot.com',
+  messagingSenderId: '168169652735',
+  appId: '1:168169652735:web:43960a19ce98ef7686a7ca',
+};
