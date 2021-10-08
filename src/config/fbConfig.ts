68375651af181959a79d738562d6c449fa7bf8cd
Lizaveta Kaliuka <lisakaliuko@gmail.com>
2021-10-08 18:05:02 +0300

    change config

--- src/config/fbConfig.ts
+++ src/config/fbConfig.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAcOSMZuSDHUMYiq5p4y_JB2IEAf1toipU',
-  authDomain: 'tracking-app-c58fb.firebaseapp.com',
-  projectId: 'tracking-app-c58fb',
-  storageBucket: 'tracking-app-c58fb.appspot.com',
-  messagingSenderId: '168169652735',
-  appId: '1:168169652735:web:43960a19ce98ef7686a7ca',
+  apiKey: 'AIzaSyClfaumbYk-rPoaM49hm_Cp6oz-PCC1v7U',
+  authDomain: 'tracking-workouts-2efd1.firebaseapp.com',
+  projectId: 'tracking-workouts-2efd1',
+  storageBucket: 'tracking-workouts-2efd1.appspot.com',
+  messagingSenderId: '169325193759',
+  appId: '1:169325193759:web:07ef9b1e040c856dd7c32a',
 };
